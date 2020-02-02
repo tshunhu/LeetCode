4439b7a151a2d5a7edf525dae3173e2cbd641465
tshunhu <tshunhu@hotmail.com>
2020-02-02 17:39:20 +0800

    新的题目

--- 121.买卖股票的最佳时机.py
+++ 121.买卖股票的最佳时机.py
@@ -7,5 +7,13 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 0 : return 0
         
+        buy_price = prices[0]
+        profit = 0
+        
+        for i in range(len(prices)):
+            buy_price = min(buy_price, prices[i])
+            profit = max(profit,  prices[i] - buy_price)
+        return profit
 # @lc code=end
